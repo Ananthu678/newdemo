2bc0d1dfef9220fb6c424cec7d9baa86a4913fbe
Ananthu678 <76966844+Ananthu678@users.noreply.github.com>
2021-01-05 01:13:33 +0530

    Update ananthu.java

--- ananthu.java
+++ ananthu.java
@@ -1,5 +1,6 @@
 class ananthu{
     public static void main(String[] args){
         System.out.println("Ananthu's first program::");
+        System.out.println("Ananthu's second program::");
 }
 }
